23c4b3acf658c5cb2df42dec18a0bb3e428ed3a1
LAPTOP-AING\HP <alwansuryadi25@gmail.com>
2025-04-15 16:49:04 +0700

    memasukan main function

--- main.go
+++ main.go
@@ -1,9 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
 
 func main() {
+	var input string
+	reader := bufio.NewReader(os.Stdin) // Membuat reader untuk membaca input
 
-	fmt.Println("Hello World")
+	for {
+		clearScreen() // Membersihkan layar sebelum menampilkan menu
+		menu()
+		fmt.Print("Masukkan pilihan: ")
+		fmt.Scan(&input)
 
+		switch input {
+		case "1":
+			clearScreen()
+			fmt.Println("case 1")
+		case "2":
+			fmt.Println("Thanks yak")
+			return
+		default:
+			fmt.Println("Pilihan tidak valid. Silakan coba lagi.")
+		}
+
+		// Menunggu pengguna untuk menekan Enter atau mengetik 'x'
+		for {
+			fmt.Println("Tekan Enter untuk kembali ke menu atau ketik 'x' untuk keluar...")
+			input, _ = reader.ReadString('\n')
+			input, _ = reader.ReadString('\n')
+			input = strings.TrimSpace(input)
+
+			if input == "" {
+				break // Kembali ke menu
+			} else if input == "x" {
+				fmt.Println("Thanks yak")
+				return // Keluar dari program
+			} else {
+				fmt.Println("Input tidak valid. Silakan coba lagi.")
+			}
+		}
+	}
+}
+
+func menu() {
+	fmt.Println(`
+===============
+DAFTAR FUNGSI
+===============
+1. FIBONACCI
+2. (x - 3) + (x - 2) + (x - 1)
+3. Fungsi nilai ke-n beda tingkat dua
+4. Eksponen 2*n
+5. Faktorial
+6. 
+7. keluar
+	`)
+}
+func clearScreen() {
+	cmd := exec.Command("cmd", "/c", "cls") // Untuk Windows
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 }
