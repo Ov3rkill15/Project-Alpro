dfc608f25c9e76d50b50de7b638331f12b724a95
zhaturra1 <alfarizifathur12@gmail.com>
2025-05-31 17:28:42 +0700

    Update alpro.go
    menambahkan case baru

--- alpro/alpro.go
+++ alpro/alpro.go
@@ -1,9 +1,17 @@
 package alpro
 
 import (
+	"Project-Alpro/alpro/function"
 	"Project-Alpro/alpro/p_array"
+	"Project-Alpro/alpro/p_maxmin"
+	"Project-Alpro/alpro/p_procedure"
+	"Project-Alpro/alpro/p_rekursif"
+	"Project-Alpro/alpro/p_searching"
+	"Project-Alpro/alpro/p_sorting"
+	"Project-Alpro/alpro/p_tipebentukan"
 	"Project-Alpro/alpro/projek_kami"
 	"Project-Alpro/atribut"
+	"Project-Alpro/contohsoal"
 	"fmt"
 
 	"github.com/common-nighthawk/go-figure"
@@ -21,15 +29,31 @@ func MainMenu() {
 			p_array.MainMenu()
 		case "2":
 			atribut.ClearScreen()
-			fmt.Println("Coming soon")
+			p_function.MainMenu()
 		case "3":
 			atribut.ClearScreen()
-			projek_kami.Project_menu()
-		case "0":
+			p_procedure.MainMenu()
+		case "4":
 			atribut.ClearScreen()
-			atribut.Loading(100)
-			fmt.Println("Menuju menu utama...")
-			return
+			p_rekursif.MainMenu()
+		case "5":
+			atribut.ClearScreen()
+			p_tipebentukan.MainMenu()
+		case "6":
+			atribut.ClearScreen()
+			p_maxmin.MainMenu()
+		case "7":
+			atribut.ClearScreen()
+			p_searching.MainMenu()
+		case "8":
+			atribut.ClearScreen()
+			p_sorting.MainMenu()
+		case "9":
+			atribut.ClearScreen()
+			projek_kami.Project_menu()
+		case "10":
+			atribut.ClearScreen()
+			contohsoal.MainMenu()
 		default:
 			atribut.ClearScreen()
 			fmt.Println("Pilihan tidak valid. Silakan coba lagi.")
@@ -46,9 +70,16 @@ func Submenu() {
 ====================================
 Selamat Datang di Algoritma dan Pemrograman
 ====================================
-1. Pembelajaran
-2. Contoh Soal
-3. Projek Kami
+1. Pembelajaran Array
+2. Pembelajaran Function
+3. Pembelajaran Procedure
+4. Pembelajaran Rekursif
+5. Pembelajaran tipeBentukan
+6. Pembelajaran maxmin
+7. Pembelajaran Searching
+8. Pembelajaran Sorting
+9. Projek kami
+10. Contoh Soal
 
 0. Keluar
 	`)
