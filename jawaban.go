37a94f122ca2a9c7df5505f721e0249d986ef59a
LAPTOP-AING\HP <alwansuryadi25@gmail.com>
2025-06-06 12:46:37 +0700

    hapus bug

--- jawaban.go
+++
@@ -1,9 +0,0 @@
-// Buatlah sebuah fungsi Go bernama `hitungJumlah` yang menerima dua bilangan bulat ($a$ dan $b$) sebagai input, lalu mengembalikan hasil penjumlahan kedua bilangan tersebut. Cetak hasil pemanggilan fungsi ini dengan input 5 dan 3.
-
-package main
-
-import "fmt"
-
-func main() {
-		fmt.Println(8)
-}
